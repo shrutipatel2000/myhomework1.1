6843a875b657e585765a4574a8f14168dc6ce3c8
shruti patel <Shrutipatel203@gmail.com>
2023-11-15 21:54:07 +0000

    mycommit

---
+++ methodeparameter.java
@@ -0,0 +1,18 @@
+package ParameterPackage;
+
+public class methodeparameter {
+    //    static parameter methode
+    //    static void mymethode(string fmodel, int year){
+    //    System.out.println(fmodel+ " "+year);
+
+    //    public.static.void.main(string[] args){
+    //   mymethode("Tesla",2022);
+
+//    Nonstatic parameter methode
+
+        void mymethode(String fmodel,int year){
+            System.out.println(fmodel+" "+year);
+        }
+
+
+}
